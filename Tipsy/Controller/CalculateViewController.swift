da302e92ae9886e7b924d7e8f9311850f92abf91
Dmitrii642 <dmit.iakovlev@gmail.com>
2023-02-14 22:05:44 +0300

    final fis

--- Tipsy/Controller/CalculateViewController.swift
+++ Tipsy/Controller/CalculateViewController.swift
@@ -19,22 +19,21 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var stepper: UIStepper!
     
   
-  var tip = 0
+    var tip = 0.0
   var numberOfGeest = 2
-  var totalSum = 0
-    var finalValue = 0
+    var totalSum = 0.0
+    var finalValue = "0.0"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("start")
+        tip = 10
+        percentLabel.text = "\(10)%"
         
         calculateBotton.layer.cornerRadius = 10
     
-       // percentLabel.text = "10%
-        
-        billTotal.keyboardType = .numberPad
+        billTotal.keyboardType = .decimalPad
        
         
         
@@ -64,7 +63,7 @@ class CalculateViewController: UIViewController {
     @IBAction func percentCountSliderAction(_ sender: UISlider) {
         
         percentLabel.text = String(format: "%.0f", sender.value)
-        tip = Int(sender.value)
+        tip = Double(Int(sender.value))
     }
     
     
@@ -73,8 +72,9 @@ class CalculateViewController: UIViewController {
         let currentSum = billTotal.text!
         
         if currentSum != "" {
-            totalSum = Int(currentSum)!
-            let result = (currentSum + (currentSum / tip)) / numberOfGeest
+            totalSum = Double(currentSum)!
+            tip = tip / 100
+            let result = totalSum * (1 + tip) / Double(numberOfGeest)
             finalValue = String(format: "%.2f", result)
         }
         
@@ -87,7 +87,7 @@ class CalculateViewController: UIViewController {
         if segue.identifier == "resultVC" {
             let destinationVC = segue.destination as! ResultViewController
             destinationVC.result = finalValue
-            destinationVC.tip = tip
+            destinationVC.tip = Int(tip * 100)
             destinationVC.split = numberOfGeest
             
         }
